131d22eae189c57d96927cb3549eb930d2b8dee5
Kushal <kushal@ualberta.ca>
2024-09-27 10:14:58 -0600

    Color Change

--- Android-Studio-Project/app/src/main/java/com/example/appify/Shape.java
+++ Android-Studio-Project/app/src/main/java/com/example/appify/Shape.java
@@ -3,6 +3,7 @@ package com.example.appify;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    String color = "blue";
     public Shape(Integer x, Integer y)
     {
         this.x = x;
