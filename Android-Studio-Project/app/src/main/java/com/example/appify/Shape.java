971ca2d76e497da607a2154f4fda4a2ed41d6fd3
ntrueman11 <113456904+ntrueman11@users.noreply.github.com>
2024-09-26 22:54:56 -0600

    Update Shape.java

--- Android-Studio-Project/app/src/main/java/com/example/appify/Shape.java
+++ Android-Studio-Project/app/src/main/java/com/example/appify/Shape.java
@@ -5,7 +5,7 @@ public abstract class Shape {
     private Integer y;
     public Shape(Integer x, Integer y)
     {
-        String color = "turqoise";
+        String color = "$PUT_YOUR_CHOICE_OF_COLOR";
         this.x = x;
         this.y = y;
     };
