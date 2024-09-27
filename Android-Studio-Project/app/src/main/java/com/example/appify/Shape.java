62cec8c876da86f0a57e972187d92d7f71e0f07f
Nathan Trueman <ntrueman@ualberta.ca>
2024-09-26 18:05:01 -0600

    Adding Project to Repo

---
+++ Android-Studio-Project/app/src/main/java/com/example/appify/Shape.java
@@ -0,0 +1,12 @@
+package com.example.appify;
+
+public abstract class Shape {
+    private Integer x;
+    private Integer y;
+    public Shape(Integer x, Integer y)
+    {
+        this.x = x;
+        this.y = y;
+    };
+    public Shape(){};
+}
